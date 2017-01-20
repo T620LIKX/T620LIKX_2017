ae4bde74b7b8ba81cf233c620f1867be2c9690b0
Steindór Tryggvason <steindortryggvason@gmail.com>
2017-01-20 21:04:53 +0000

    laga smá villu

--- Into_PSQL.py
+++ Into_PSQL.py
@@ -4,7 +4,7 @@ import matplotlib.pylab as plt
 from sqlalchemy import create_engine
 #Línur sem eru kommentaðar út innihalda kóða þar sem hver notandi þarf að gera breytingar
 Data = pd.DataFrame()
-#Data=pd.read_excel('C:\data\DataA.xlsx', skiprows=3,parse_cols='B:P')  #skrá sett inn í pandas dataframe - athugið slóð að skrá er breytileg
+Data=pd.read_excel('C:\data\DataA.xlsx', skiprows=3,parse_cols='B:P')  #skrá sett inn í pandas dataframe - athugið slóð að skrá er breytileg
 
 #Breytum íslenskum stöfum í dálka nöfnum og línubil tekinn út 
 cols = Data.columns
@@ -12,8 +12,8 @@ cols = cols.map(lambda x: x.replace(' ', '_').replace('á','a').replace('Á','A'
 Data.columns = cols
 #íslenskir stafir teknir úr daga dálki
 dag = Data.Dagur
-dag =dag.map(lambda x: x.replace('ö','o').replace('á','a').replace('ð','d'))
+dag =dag.map(lambda x: x.replace('ö','o').replace('á','a').replace('ð','d').replace('þ','t'))
 Data.Dagur=dag
 print(Data)
-#engine = create_engine('postgresql://USERNAME:PASSWORD@localhost:5432/DATABASE') # býr til töflu með öllum gögnunum í postres þarf að setja inn notendanafn, password og nafn á gagnagrunni
-#Data.to_sql('data', engine)
+engine = create_engine('postgresql://USERNAME:PASSWORD@localhost:5432/DATABASE') # býr til töflu með öllum gögnunum í postres þarf að setja inn notendanafn, password og nafn á gagnagrunni
+Data.to_sql('data', engine)
