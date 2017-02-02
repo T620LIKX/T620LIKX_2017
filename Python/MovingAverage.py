15a8c7af971762daf518a5cf13cc9bef832c9097
Vésteinn Sigurjónsson <vesteinnsigurjonsson@Vesteinns-MacBook-Air.local>
2017-02-02 17:56:21 +0000

    Moving Average

--- Python/MovingAverage.py
+++ Python/MovingAverage.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pylab as plt 
+import matplotlib.pylab as plt
+import csv 
 from sqlalchemy import create_engine
-#Línur sem eru kommentaðar út innihalda kóða þar sem hver notandi þarf að gera breytingar
-def movingaverage(df):
-	vikur = int(input('Hversu margar vikur viltu? '))
-	dagar = vikur*5
+
+def movingaverage(df,vikur):
+	dagar = vikur*7
 	endir = df["Símtöl inn"].count()
 	byrjun = endir-dagar
 	summa = 0
@@ -15,13 +15,53 @@ def movingaverage(df):
 	spa = summa/vikur
 	return spa
 
+def movingaverage2(df, byrja, enda):
+	cumsum = 0
+	for i in range (byrja,enda):
+		cumsum = cumsum + df.loc[i,['Símtöl inn']]
+	spa = cumsum/(enda-byrja)
+	return spa
 
 df = pd.DataFrame()
 df = pd.read_excel('/Users/vesteinnsigurjonsson/Desktop/HR/6Vor 2017/Líkan X/T620LIKX_2017/Python/DataA.xlsx', skiprows=3,parse_cols='B:P'.lower()) 
 
-spa = movingaverage(df)
 
-#df.append(pd.DataFrame([spa],columns=str("Símtöl inn")))
-print('I naestu viku munu liklega %d simtol berast' %(spa) )
-print('Á hverjum degi munu %d simtol berast' %(spa/7))
+byrjun = 3
+endir = df["Símtöl inn"].count()
+lis = []
+
+for i in range (byrjun,endir):
+	spa = 0
+	spa = movingaverage2(df,i-3,i)
+	lis.append(spa)
+
+tuple(lis)
+df.set_index(['Símtöl inn']).to_records().tolist()
+
+plt.plot(lis,'b')
+plt.plot(df["Símtöl inn"],'r')
+plt.ylabel('Símtöl')
+plt.xlabel('Dagar')
+plt.show()
+#plt.plot(range(0,len(df["Símtöl inn"])))
+#plt.plot(range(3,len(spa)+3))
+#plt.show()
+
+
+
+#M=[];
+#V=0;
+#for i in range(21,len(df.index)):
+#	V=V+df.loc[i,"Símtöl inn"]
+#	if df.loc[i,"Klukkustund"]==21:
+#		M.append(V)
+#		V=0
+
+#spagildi = movingaverage(df,3)
+
+#print(spagildi)
+
+
+
+
 
