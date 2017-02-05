e58142c2227215b07a12b25a304a7643f1c47e8d
Davíð Mollberg <Dav Mollberg>
2017-02-05 17:13:41 +0000

    dd

--- Python/GogD/Hermun.py
+++ Python/GogD/Hermun.py
@@ -17,20 +17,24 @@ def StartDay():
 	"""
 	Fall sem "opnar" innhringiverið og biður notendan að stilla upp vaktaplaninu á starfsmönnunum og skilar
 	út lista af dictonarys af starfsmönnunum.
+	WorkingOn: Id á símtali ef er að þjónusta
+			   Idle ef laus   
+	IsWorking: Yes ef starfsmaðurinn er á vakt
+			   No ef starfsmaðurinn er búinn að vakt eða ekki byrjaður.
 	"""
 
 	global clock
-	clock = 0 #Núllstillir klukkuna.
+	clock = 0 #Núllstillir klukkuna í upphafi dags.
 
 	ListOfStaff = []
 	for i in range(GeneralInfo["NumberOfStaff"]):
 		#begin = int(input('Staff ' + str(i+1) + ' begins: '))
 		#end = int(input('Staff ' + str(i+1) + ' ends: '))
-
-		begin = 0
+		begin = 0 #harðkóða upphafs og endatíma fyrir starfsmanninn til að byrja með.
 		end = 1000
+
 		staffId = "StaffId" + str(i+1)
-		staffId = {"StaffId": staffId,"Begins": begin, "Ends": end, "WorkinOn": "NAN", "Idel": "No"}
+		staffId = {"StaffId": staffId,"Begins": begin, "Ends": end, "WorkinOn": "idle", 'IsWorking': "No"}
 		ListOfStaff.append(staffId)
 	return ListOfStaff;
 
@@ -53,8 +57,8 @@ def GeneratePhoneCalls():
 
 	millitimi = np.random.poisson(lam=5.0, size=100) #Fj0ldi simtala a dag er harðkóðað sem 100
 
-	ID=0
-	simtalinn_sek=0
+	ID = 0
+	simtalinn_sek = 0
 	simtol=[]
 	for i in millitimi:
 		simtalinn_sek = simtalinn_sek + millitimi[i] #halda utan um hvern símtal kom inn.
@@ -63,22 +67,47 @@ def GeneratePhoneCalls():
 		simtol.append(simtal) #búa til lista með öllum símtölunum
 	return simtol;
 
-def UpdateStatus():
+def UpdateStatusOfPhoneCall():
 	for i in ListOfPhoneCalls:
 		if i["simtal_inn"] <= clock:
 			i["status"] = "B"
 
+def UpdateStatusOfEmployee()
+	for i in ListOfStaff:
+		if i[""]
+#============================================================================
+'''
+def find_staff():
+	"""
+	þessi forlykkja fer í gegnum listan staff finnur dict fyrir einn starfsmann og skilar id fyrir starfsmann ef hann er laus
+   	"""
+	for i in ListOfStaff: 
+		if i['Idel'] == 'yes':
+			return(i['StaffId']);
+		else:
+			return(); #allir starfsmennuppteknir hvað þá ?
 
+def find_next_phonecall(): 
+#þessi forlykkja fer í gegnum listan simtol finnur dict fyrir eitt simtal og skilar id fyrir símtali ef simtal er í bið
+   for x in enumerate(simtol):#kalla í listan af símtölunum
+      
+      if x.get('stada') =='waiting':
+         return(x.get('ID'))# skilar simtal id
+      else:
+         return() # ekkert símtal í bið hvað þá ?
+
+def set_employer_call():
+   next_phonecall=find_next_phonecall(simtol)
+   free_staff=find_staff(ListOfStaff)
+   []
+'''
 #------------------------------------------------------------------------------------------------------------------------------------
 
 ListOfStaff = StartDay()
 ListOfPhoneCalls = GeneratePhoneCalls()
 
 while clock <= GeneralInfo["CloseAt"]:
-	#print(clock)
-	UpdateStatus()
+
+	UpdateStatusOfPhoneCall()
 	UpdateClock(1)
 
-
-for i in ListOfPhoneCalls:
-	print(i)
