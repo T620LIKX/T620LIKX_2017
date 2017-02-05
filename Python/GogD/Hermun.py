d6e1be8be30f167190990e1229c6967321dc2b49
Davíð Mollberg <Dav Mollberg>
2017-02-05 16:36:03 +0000

    s

--- Python/GogD/Hermun.py
+++ Python/GogD/Hermun.py
@@ -1,30 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt 
+import scipy.optimize as sci
+import math
+
+#======================================================================
+GeneralInfo = {"OpenAt":  0, "CloseAt": 1000, "NumberOfStaff": 1} #	Note, seinna meir væri hægt að gera þetta að falli með user interface-i
+
 clock = 0
 
 def UpdateClock(n):
 	global clock
 	clock = clock + n
 
-def MakeSimtal(id):
-	key = "PhoneCallNr" + id
-	key = {"LengdThjonustu": 5, "BidTimi": 0, "HvarErEg": "A"}
-	return;
-
-def StartDay():
+def StartDay(GeneralInfo):
 	"""
 	Fall sem "opnar" innhringiverið og biður notendan að stilla upp vaktaplaninu og skilar
 	út lista af starfsmönnunum.
-	Note, seinna meir væri hægt að setja þetta upp með user interface-i
 	"""
-	ListOfStaff = []
-	#staff = int(input("Enter number of employees: "))
+	
 	global clock
-	clock =0
+	clock = 0 #Núllstillir klukkuna.
 
-
-	staff = 1
-	for i in range(staff):
+	ListOfStaff = []
+	for i in range(GeneralInfo["NumberOfStaff"]):
 		#begin = int(input('Staff ' + str(i+1) + ' begins: '))
 		#end = int(input('Staff ' + str(i+1) + ' ends: '))
+
 		begin = 0
 		end = 1000
 		staffId = "StaffId" + str(i+1)
@@ -32,6 +34,53 @@ def StartDay():
 		ListOfStaff.append(staffId)
 	return ListOfStaff;
 
-ListOfStaff = StartDay()
+def nytt_simtal(ID,simtal_inn):
+	"""
+	Býr til nýtt símtal og upphafstillir breytur.
+	Breytan status heldur utan um í hvaða fasa símtalið er.
+	A: Er ekki búinn að hringja.
+	B: Er í röð.
+	C: Er í þjónustu.
+	D: Er búinn að fá þjónustu.
+	"""
 
-print(ListOfStaff)
+	lengd_simtal= int(np.random.exponential(scale=100, size=None)) # beta =100 parameter fyrir exponential 1/landa
+
+	simtal = {"ID": ID, 'lengd_simtals':lengd_simtal,'status': "A", 'simtal_inn': simtal_inn, 'simtal_lokid': (simtal_inn+lengd_simtal)}
+	return (simtal);
+
+def GeneratePhoneCalls():
+
+	millitimi = np.random.poisson(lam=5.0, size=100) #Fj0ldi simtala a dag er harðkóðað sem 100
+
+	ID=0
+	simtalinn_sek=0
+	simtol=[]
+	for i in millitimi:
+		simtalinn_sek = simtalinn_sek + millitimi[i] #halda utan um hvern símtal kom inn.
+		simtal = nytt_simtal(ID, simtalinn_sek) #býr til dict fyrir símtal
+		ID += 1 
+		simtol.append(simtal) #búa til lista með öllum símtölunum
+	return simtol;
+
+def UpdateStatus(ListOfPhoneCalls):
+	for i in ListOfPhoneCalls:
+		if i["simtal_inn"] <= clock:
+			i["status"] = "B"
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+ListOfStaff = StartDay(GeneralInfo)
+
+ListOfPhoneCalls = GeneratePhoneCalls()
+
+#print(ListOfStaff)
+#print(ListOfPhoneCalls)
+#for i in ListOfPhoneCalls:
+#	print(i)
+
+UpdateStatus(ListOfPhoneCalls)
+#print('l=========================')
+#for i in ListOfPhoneCalls:
+#	print(i)
+
