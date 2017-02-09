d0e59fd1fa7efc8ab5560032e9bd1f11f45d5f32
Davíð Mollberg <Dav Mollberg>
2017-02-09 23:37:25 +0000

    dsad

--- Python/GogD/Hermun2.0.py
+++ Python/GogD/Hermun2.0.py
@@ -7,6 +7,8 @@ import math
 #======================================================================
 GeneralInfo = {"OpenAt":  0, "CloseAt": 500, "NumberOfStaff": 1} #Note: seinna meir væri hægt að gera þetta að falli með user interface-i
 EventList = [] #Gæti verið óþarfi að declarea hann hérna. Tékka á því.
+ListOfStaff = [] #Gæti verið óþarfi að declarea hann hérna, geri það nú þegar í fallinu setstaff
+
 clock = 0
 
 def Start_Day():
@@ -17,54 +19,15 @@ def Start_Day():
 	global clock
 	clock = 0 #Núllstillir klukkuna í upphafi dags. Just in case.
 	
-	At_Event('opens', GeneralInfo['OpenAt'])
-	At_Event('closes', GeneralInfo['CloseAt'])	
+
+	At_Event('opens', GeneralInfo['OpenAt'], 0, 0)
+	At_Event('closes', GeneralInfo['CloseAt'], 0, 0)
 
 	return;
 
-
-def Set_Staff():
-	"""
-	Fall sem upphafsstillir þá starfsmenn sem eru á vakt þennan daginn og setur það inn sem event.
-	WorkingOn: Id á símtali ef er að þjónusta
-			   idle ef laus   
-
-	IsWorking: yes ef starfsmaðurinn er á vakt
-			   No ef starfsmaðurinn er búinn að vakt eða ekki byrjaður.
-
-	Note: Hérna væri hægt að bæta við kaffipásum starfsmannsins, þynnkustuðul o.s.fr.
-	"""
-
-	ListOfStaff = []
-	for i in range(GeneralInfo["NumberOfStaff"]):
-		#begin = int(input('Staff ' + str(i+1) + ' begins: '))
-		#end = int(input('Staff ' + str(i+1) + ' ends: '))
-		begin = 0 #harðkóða upphafs og endatíma fyrir starfsmanninn til að byrja með.
-		end = 500
-		staffId = {"Type": "Staff", "StaffId": i, "Begins": begin, "Ends": end, "WorkinOn": "idle", 'IsWorking': "no"}
-		ListOfStaff.append(staffId)
-
-	At_Event("Staff", ListOfStaff) 
-
-	return;
-
-
-def nytt_simtal(id,simtalInn):
-	"""
-	Býr til nýtt símtal og upphafstillir breytur.
-	
-	ATH það á eftir að útfæra leiðina til að halda utanum ID
-
-	"""
-
-	lengdSimtal= int(np.random.exponential(scale=100, size=None)) # beta =100 parameter fyrir exponential 1/landa
-	simtal = {"ID": ID,'Lengd_Simtals':lengdSimtal, 'callInn': simtalInn, }
-	return (simtal);
-
-
-def At_Event(EventType, time):
+def At_Event(EventType, time, id, other):
 	'''
-	Event listi.
+	Event listi. Hér  bætum við eventum inn á listann
 
 	Innhringiver opnar: 		opens
 	InnhringiVer Lokar:			closes
@@ -76,32 +39,114 @@ def At_Event(EventType, time):
 
 	'''
 
-	'''
-	if EventType == 'opens'
-
-	elif EventType == 'closes'
-			EventList.append({"Type": EventType, "Time": time})
-	elif EventType == 'starts'
-
-	elif EventType == 'ends'
-
-	elif EventType == 'callInn'
-
-	elif EventType == 'callEnds'
-
-	else
-	'''
-
-	EventList.sort(key = {lambda x:x["Time"]}) #Major key. Fall sem raðar listanum yfir í evnets í rétta tíma röð þannig að Eventlist[0] er alltaf næst hluturinn í réttri tímaröð
-
+	if EventType == 'opens' or EventType == 'closes':
+		EventList.append({"Type": EventType, "Time": time})
 	
+	elif EventType == "starts" or EventType == 'ends':
+		EventList.append({'Type': EventType, 'Time': time, 'Id': id})
+
+	elif EventType == "callInn":
+		EventList.append({"Type": EventType, "Time": time, 'Id': id, "CallLength": other})
+	
+	elif EventType == "callEnds":
+		Eventlist.append({"Type": EventType, "Time": time, 'Id': id})
+
+	'''
+	elif EventType == 'callEnds'
+	'''
+	
+	EventList.sort(key=lambda x: x["Time"]) #Major key. Fall sem raðar listanum yfir í evnets í rétta tíma röð þannig að Eventlist[0] er alltaf næst hluturinn í réttri tímaröð
+	return;
+
+def Set_Staff():
+	"""
+	Fall sem upphafsstillir þá starfsmenn sem eru á vakt þennan daginn og setur það inn sem event.
+	WorkingOn: Id á símtali ef er að þjónusta
+			   idle ef laus   
+
+	Note: Hérna væri hægt að bæta við kaffipásum starfsmannsins, þynnkustuðul o.s.fr.
+	"""
+
+	for i in range(GeneralInfo["NumberOfStaff"]):
+		#begin = int(input('Staff ' + str(i+1) + ' begins: '))
+		#end = int(input('Staff ' + str(i+1) + ' ends: '))
+		begin = 0 #harðkóða upphafs og endatíma fyrir starfsmanninn til að byrja með.
+		end = 450
+		staffId = {"Type": "Staff", "StaffId": i}
+		#staffId = {"Type": "Staff", "StaffId": i, "Began": 0, "Ended": 0, "WorkinOn": "idle"}
+		ListOfStaff.append(staffId)
+		At_Event("starts", begin, i, 0)
+		At_Event("ends", end, i, 0)
+
+	return;
+
+def find_staff(id):
+	for i in ListOfStaff:
+		if i["StaffId"] == id:
+			return i;
+
+
+def GeneratePhoneCalls():
+	'''
+	Þetta fall býr til lista af dictonarys af símtölum yfir daginn með því að nota fallið nytt_simtal.
+	Note: Hérna væri hægt að útfæra frekari líkindadreifinu á símtölinn þegar það er fyrir hendi
+	'''
+
+	millitimi = np.random.poisson(lam=5.0, size=100) #Fjldi simtala a dag er harðkóðað sem 100
+
+	ID = 0
+	simtalinn_sek = 0
+	for i in millitimi:
+		simtalinn_sek = simtalinn_sek + millitimi[i]
+		lengdSimtal= int(np.random.exponential(scale=100, size=None))
+		At_Event("callInn", simtalinn_sek, ID, lengdSimtal)
+		ID += 1 
+	return;
+
+
 #----------------------------------main----------------------------------------------
 
+Start_Day()
+Set_Staff()
+GeneratePhoneCalls() #Væri mjög töff ef þetta fall yrði meira discrete
 
-while clock <= GeneralInfo["CloseAt"]:
+while clock < GeneralInfo["CloseAt"]:
 	e = EventList[0]
 	del EventList[0]
+
 	clock = e["Time"]
+
+
+	if e["Type"] == "opnes":
+		clock = e['Time']
+
+	elif e["Type"] == "closes":
+		clock == e['Time']
+	elif e["Type"] == 'starts':
+		'''
+		Byrjar að uppfæra breytuna um starfsmanninn, þ.e.a.s 
+		x1. finna starfsmann í listofstaff
+		x2. Setja inn key i dictid sem er "began": e['Time']
+		x3. Setja inn key sem er "WorkingOn": "idle"
+		4. Tekka hvort starfsmadur getur svarad simtali
+		'''	
+		find_staff(e["Id"]).update({"Began": e["Time"], "WorkingOn": "idle"})
+		clock == e['Time']
+
+
+	elif e["Type"] == 'ends':
+		find_staff(e["Id"]).update({"Ended": e["Time"]})
+		clock == e['Time']
+
+	elif e["Type"] == 'callInn':
+		e["Status"] = 'innLine'
+		clock == e['Time']
+
+	elif e["Type"] == "callEnds":
+		e["Status"] = "callHasEnded"
+		clock == e['Time']
+				
+	print(clock)
 
 '''
 	if e["Type"] == eitthvad
@@ -117,4 +162,4 @@ while clock <= GeneralInfo["CloseAt"]:
 
 	else
 
-'''
+'''	
