5448ea4ad7b987d21a7e2bb04cc463b04b172d54
Steindor <steindortryggvason@gmail.com>
2017-02-20 17:01:13 +0000

    smá breytingar útaf renenging

--- Python/Simulator/statistics_manager.py
+++ Python/Simulator/statistics_manager.py
@@ -1,4 +1,4 @@
-
+ 
 class StatisticsManager:
     # stats
 
@@ -11,7 +11,7 @@ class StatisticsManager:
         self.average_sojourn = 0
         self.average_wait = 0
         self.max_wait = 0
-
+        self.phonecalls_answered = 0
 
     def update_statistics(self, currenttime, lasttime, events, phonecalls, workers, settings):
 
@@ -21,8 +21,12 @@ class StatisticsManager:
         self.max_queue_length = max( self.max_queue_length, phonecalls.length())
 
     def calculate_statistics(self, phonecalls, workers, settings):
-        self.phonecall_counter = len(phonecalls.finished_phonecalls)
+        self.phonecall_counter = len(phonecalls.finished_phonecalls) + len(phonecalls.reneging_phonecalls)
+        self.phonecalls_answered = len(phonecalls.finished_phonecalls)
         self.finished_phonecalls = phonecalls.phonecall_id
+        self.reneging_phonecalls = len(phonecalls.reneging_phonecalls)
+        print (self.phonecall_counter)
+        print (self.reneging_phonecalls)
         for p in phonecalls.finished_phonecalls:
             self.average_sojourn += p['end time'] - p['arrival']
             self.average_wait += p['answer time'] - p['arrival']
