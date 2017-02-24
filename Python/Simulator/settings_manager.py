60d1724b0e382f38cf3c1843dadda54d04d54a53
Rebekka Jóhannsdóttir <rebekkajohannsdottir@Rebekka-Johannsdottirs-MacBook-Pro.local>
2017-02-24 15:52:02 +0000

    post processing baett vid

--- Python/Simulator/settings_manager.py
+++ Python/Simulator/settings_manager.py
@@ -10,6 +10,8 @@ class SettingsManager():
         self.lam = 0.1
         self.mu = 0.2
         self.rho = self.lam / self.mu
+        self.muj_process = 0.1                             ### bætti þessu við // rebekka
+        self.std_process = 0.005                           ### bætti þessu við // rebekka
 
     def rand_phonecall_length(self):
         return numpy.random.exponential(1/self.mu)
@@ -19,6 +21,8 @@ class SettingsManager():
     def rand_reneg_time(self):  # á eftir að breyta
         return 5
         #return self.endtime+1
+    def rand_processing_time(self):                                                 #baetti thessu vid
+        return numpy.random.normal(self.muj_process, self.std_process, 1000)
 
 
 
