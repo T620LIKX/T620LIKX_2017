92923b53c9d36917ee756545e43eb8f05a2acf6d
Vésteinn Sigurjónsson <vesteinn01@gmail.com>
2017-03-10 14:37:58 +0000

    nýr kóði

---
+++ Python/Simulator/gettinglambdas.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import simulator
+import csv
+
+
+def CollectList(filename, day):
+	lambdas = []
+
+	file = open(filename,"r")
+	reader = csv.reader(file)
+	data = []
+	for row in reader:
+		data.append(row)
+	thelam = ''
+
+
+	for i in range (0,len(data)):
+		dagur = data[i][0]
+		if dagur == day:
+			thelam = float(data[i][1].split('|')[1].strip())
+			thelam = thelam/3600
+			timi = float(data[i][1].split('|')[0].strip().replace(')',''))
+			timi = timi*3600
+			lambdas.append( {'time': timi, 'lam': thelam} )
+	
+	file.close()
+	return lambdas
+
+
+
+sim_settings = {}
+sim_settings['start time'] = 32400
+sim_settings['end time'] =  75600
+
+
+iterations = 5
+
+days = ['man','tri','mid','fim','fos','lau','sun']
+for d in days:
+	lambdas = CollectList('MedaltalPerHour.csv',d)
+
+	for i in range(iterations):
+		tmplamdas = lambdas.copy()
+		stats = simulator.run_simulation(settings_details = sim_settings, lambdas = tmplamdas, SHOWGRAPH=False)
+
