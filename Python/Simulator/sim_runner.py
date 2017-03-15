690d1ba297b78d3fd170a123da342beaccd83be1
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-03-15 15:31:09 +0000

    Creating initalization for company A

--- Python/Simulator/sim_runner.py
+++ Python/Simulator/sim_runner.py
@@ -3,36 +3,24 @@ import gettinglambdas
 import worker_details
 import simulator
 import helperfunctions as hf
+import company_a
+import company_b
+import numpy
 
 
-sim_settings = {}
-sim_settings['start time'] = hf.strtime2sec('9:00') #32400
-sim_settings['end time'] =  hf.strtime2sec('21:00') #75600
-
-shift_times = [ {'starttime': hf.strtime2sec('9:00'), 'endtime': hf.strtime2sec('21:00') },
-                {'starttime': hf.strtime2sec('12:00'), 'endtime': hf.strtime2sec('21:00') },
-                {'starttime': hf.strtime2sec('9:00'), 'endtime': hf.strtime2sec('15:00') },
-                {'starttime': hf.strtime2sec('10:00'), 'endtime': hf.strtime2sec('14:00') },
-                {'starttime': hf.strtime2sec('9:00'), 'endtime': hf.strtime2sec('21:00') } ]
+days = ['mon','tue','wed','thu','fri','sat','sun']
+sim_settings, lambdas, workers = company_a.initialize_company(days)
 
 iterations = 5
 
-
-results = []
-
-days = ['man','tri','mid','fim','fos','lau','sun']
+results = {}
 
 for d in days:
-    lambdas = gettinglambdas.CollectList('MedaltalPerHour.csv', d)
-
-    workers = worker_details.get_workers(shift_times)
+    results[d] = []
 
     for i in range(iterations):
-        tmplamdas = lambdas.copy()
-        stats = simulator.run_simulation(settings_details = sim_settings, lambdas = tmplamdas, workers_details = workers, SHOWGRAPH=False, SHOWOUTPUT='.')
-        stats.info = d
-
-        results.append(stats)
+        stats = simulator.run_simulation(settings_details = sim_settings[d].copy(), lambdas = lambdas[d].copy(), workers_details = workers[d].copy(), SHOWGRAPH=False, SHOWOUTPUT='.')
+        results[d].append(stats)
 
 print('')
 
