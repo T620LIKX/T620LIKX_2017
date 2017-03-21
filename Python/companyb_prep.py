7a961f3f8529dd73ce42a5a60c974f8feb8e3132
gussi94 <gudjoni14@ru.is>
2017-03-21 22:40:32 +0000

    styrmir

--- Python/companyb_prep.py
+++ Python/companyb_prep.py
@@ -1,7 +1,31 @@
 from sqlalchemy import Table, MetaData, create_engine
-import pandas
-engine = create_engine("postgresql://postgres:st36543654@localhost/likanx") # breyta hjá hverjum
+import datetime
+import calendar
+import numpy as np
+import pandas as pd
+engine = create_engine("postgresql://postgres:lukas@localhost/likanx") # breyta hjá hverjum
     
-Table=pandas.read_sql_table('callcenter_b',engine)
+Table=pd.read_sql_table('callcenter_b',engine)
 
-print(Table['calls_queued'].sum())
+timestamp=Table.ix[:,0]
+## búum til nýtt data frame sem er innihaldið af deginum þurfum að púsla því saman við hitt
+gogn=pd.DataFrame({"ar": timestamp.dt.year,
+              "manudur": timestamp.dt.month,
+            #  "dagsetning": timestamp.dt.day,
+              "klukkustund": timestamp.dt.hour,
+            #  "dayofyear": timestamp.dt.dayofyear,
+              "week": timestamp.dt.week,
+              "vikunumer": timestamp.dt.weekofyear,
+              "dayofweek": timestamp.dt.dayofweek,
+              "weekday": timestamp.dt.weekday,
+            #  "quarter": timestamp.dt.quarter,
+             })
+# print(gogn)
+# print(Table)
+# print(type(gogn))
+# print(type(Table))
+
+# það þarf að skoða þetta, misssum ehv tölur 
+bigdata=Table.append(gogn)
+print(bigdata)
+
